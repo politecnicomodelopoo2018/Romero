5297b4fc919b7b9efbc14825a8d1c9d5ae1879f6
tobiasrf22 <tobiasrf22@gmail.com>
2018-05-11 10:59:44 -0300

    asd

--- Flask.py
+++ Flask.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "GSW CAMPEON"
+    return "GSW CAMPEON" , 'asdsad'
+
 if __name__ == "__main__":
     app.run(debug=True)
