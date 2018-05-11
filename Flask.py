360b182defdc2e6ccc0b9f88782c1c8e69a966a1
tobiasrf22 <tobiasrf22@gmail.com>
2018-05-11 11:18:05 -0300

    asd

--- Flask.py
+++ Flask.py
@@ -1,8 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "GSW CAMPEON" , 'asdsad'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run()
